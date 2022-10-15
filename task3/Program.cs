0ed3140a47c50796b162e9957445a207dcab0d1a
Alexander Shar <alahan05@gmail.com>
2022-10-15 11:02:48 +0300

    3rd task ended

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 3
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+int Prompt()
+{
+    Console.WriteLine("Write number >");
+    string inputString = Console.ReadLine();
+    int value = Convert.ToInt32(inputString);
+    return value;
+}
+int number = Prompt();
+int index = 1;
+while(index <= number)
+{
+    System.Console.Write($"{index * index * index}, ");
+    index++;
+}
